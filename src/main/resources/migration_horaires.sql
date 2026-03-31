2b7afde432994943f5ed9a40b729bcafda6db0a8
latifa <taybilatifa46@gmail.com>
2026-03-31 02:25:34 +0100

    chore: remove SQL initialization scripts to fix CI build

--- src/main/resources/migration_horaires.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Augmenter la limite de horairesOuverture
--- Date: 2026-03-27
--- Description: Standardized for PostgreSQL (ALTER COLUMN ... TYPE TEXT)
-
-ALTER TABLE centre_collecte
-ALTER COLUMN horaires_ouverture TYPE TEXT;
-
--- PostgreSQL doesn't use DESCRIBE, we can check via information_schema or \d if needed manually
